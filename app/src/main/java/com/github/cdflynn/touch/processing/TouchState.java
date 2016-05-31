bd380dccfb76508be3b3c018c16eded833feed90
Collin Flynn <cflynn@duffy.com>
2016-05-31 12:48:26 -0500

    comments

--- app/src/main/java/com/github/cdflynn/touch/processing/TouchState.java
+++ app/src/main/java/com/github/cdflynn/touch/processing/TouchState.java
@@ -4,13 +4,35 @@ package com.github.cdflynn.touch.processing;
  * Container for holding relevant details about any in-progress motion events.
  */
 public class TouchState {
-    public static final float NONE = -1f;
+    /**
+     * No-Value for state.  Typically the {@link TouchState} fields will hold this value
+     * if no touch event is in progress.
+     */
+    public static final float NONE = Float.NaN;
+    /**
+     * The relative x coordinate where the motion event started.
+     */
     public float xDown = NONE;
+    /**
+     * The relative y coordinate where the motion event started.
+     */
     public float yDown = NONE;
+    /**
+     * The current x coordinate
+     */
     public float xCurrent = NONE;
+    /**
+     * The current y coordinate
+     */
     public float yCurrent = NONE;
+    /**
+     * The distance between the down and current coordinates.
+     */
     public float distance = NONE;
 
+    /**
+     * 
+     */
     public void reset() {
         xDown = NONE;
         yDown = NONE;
